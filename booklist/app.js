b8021a245bd51ba0fa3eac9ccb44fd8e96b9c209
Hoa Ngo <hoango1607@gmail.com>
2020-08-27 10:29:52 +0700

    add basic functions

--- booklist/app.js
+++ booklist/app.js
@@ -41,6 +41,31 @@ class UI {
 
     list.appendChild(row);
   }
+
+  static deleteBook(el) {
+    if (el.classList.contains('delete')) {
+      el.parentElement.parentElement.remove();
+    }
+  }
+
+  static showAlert(message, className) {
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+
+    // Show alert message
+    div.appendChild(document.createTextNode(message));
+
+    // Put alert between container and #book-form
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
+  }
+
+  static clearFields() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#isbn').value = '';
+  }
 }
 
 // Store Class: Handles storage (local)
@@ -49,5 +74,30 @@ class UI {
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add A Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Get form values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const isbn = document.querySelector('#isbn').value;
+
+  // Validate
+  if (title === '' || author === '' || isbn === '') {
+    UI.showAlert('Please fill in all fields', 'danger');
+  } else {
+    // Instatiate book
+    const book = new Book(title, author, isbn);
+
+    // Add book to UI
+    UI.addBookToList(book);
+
+    // Clear fields
+    UI.clearFields();
+  }
+});
 
 // Event: Remove a Book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+  UI.deleteBook(e.target);
+});
